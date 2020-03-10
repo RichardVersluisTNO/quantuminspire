c5fa7208ace8cf7b63de5b325598760339b3b4d1
Marten Arthers <marten@Martens-MacBook-Pro.local>
2020-03-10 22:00:19 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -94,4 +94,4 @@ setup(name='quantuminspire',
       packages=['quantuminspire', 'quantuminspire.qiskit', 'quantuminspire.projectq'],
       install_requires=['pytest>=3.3.1', 'coverage>=4.5.1', 'matplotlib>=2.1',
                         'coreapi>=2.3.3', 'numpy', 'jupyter', 'nbimporter', 'sklearn'],
-      extras_require={'qiskit': ["qiskit>=0.9.0"], 'projectq': ["projectq>=0.4"], extras_require})
+      extras_require={'qiskit': ["qiskit>=0.9.0"], 'projectq': ["projectq>=0.4"]}.update(rtd_requires))
